d02cd34696544f19b7c4a47e5ebad106def5c8a0
Jules Telan <julio_telan@dlsu.edu.ph>
2018-02-27 14:07:59 +0800

    Working Products Page

--- src/products/components/products.jsx
+++ src/products/components/products.jsx
@@ -2,13 +2,20 @@
 import * as ProductsActions from "../actions/products";
 import React from "react";
 import { connect } from "react-redux";
-// import {Row, Col, Button, Navbar, MenuItem, Nav, NavItem, NavDropdown} from "react-bootstrap";
+import { Table } from "react-bootstrap";
 
 class Products extends React.Component {
+    componentWillMount() {
+        this.props.getLiveProducts();
+    }
+
     render() {
+        let productCategoryRow = this.props.products.map((product, i) => {
+            return <ProductsCategoryRow key={"product_category_row_" + i} product={product} />
+        });
         return (
             <div>
-                Products Page
+                {productCategoryRow}
             </div>
         );
     }
@@ -25,5 +32,32 @@ const VisibleProducts = connect(
     mapStateToProps,
     ProductsActions
 )(Products);
+
+const ProductsCategoryRow = ({product}) => {
+    let productsRow = product.Products !== null ? product.Products.map((product, i) => {
+        return <ProductsRow product={product} key={"products_row_" + i} />
+    }) : null;
+    return(
+        <div>
+            <h4>{product.CategoryName}</h4>
+            <Table>
+                <tbody>
+                    {productsRow}
+                </tbody>
+            </Table>
+        </div>
+    )
+}
+
+const ProductsRow = ({product}) => {
+    console.log("props", product)
+    return (
+        <tr>
+            <td>{product.name}</td>
+            <td>{product.modelId}</td>
+            <td>{product.price}</td>
+        </tr>
+    )
+}
 
 export default VisibleProducts;
